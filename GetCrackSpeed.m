7fb91277b82e66057c123be5b7d932352a877e56
woodsss-chen <43072100+woodsss-chen@users.noreply.github.com>
2018-10-02 15:05:36 -0400

    Add files via upload
    spline version

--- GetCrackSpeed.m
+++ GetCrackSpeed.m
@@ -3,27 +3,20 @@ dir = 'G:\My Drive\Dasgupta Research Group\Composites Subgroup\Data\Wedge Test\Y
 filename ='20180917.xlsx';
 filename = strcat(dir,'\',filename);
 Data = xlsread(filename,'0910BcMat');
-time = Data(3:31,2);
-length = Data(3:31,3);
+time = Data(5:31,2);
+length = Data(5:31,3);
 logtime = log(time);
-f = fit(logtime,length,'poly5');
-p = coeffvalues(f);
+fittime = min(logtime):0.3:max(logtime);
+fitlength = spline(logtime,length,fittime);
 
-syms flength(x);
-flength(x) = p(1)*log(x)^5 + p(2)*log(x)^4 + p(3)*log(x)^3 + p(4)*log(x)^2 + p(5)*log(x) + p(6);
+plot(logtime,length,'o',fittime,fitlength);
 
-syms fspeed(x);
-fspeed(x) = diff(flength(x));
-
-fittime = min(time):20:3e4;
-fitspeed = double(fspeed(fittime));
-
-figure(1)
-loglog(time,length,'o',time,flength(time),'LineWidth',1);
-xlabel('time [s]');
-ylabel('crack length [mm]');
-
-figure(2)
-loglog(fittime,fitspeed,'LineWidth',1);
-xlabel('time [s]');
-ylabel('crack length [mm/s]');
+% figure(3)
+% loglog(time,length,'o',time,flength(time),'LineWidth',1);
+% xlabel('time [s]');
+% ylabel('crack length [mm]');
+% 
+% figure(4)
+% loglog(fittime,fitspeed,'LineWidth',1);
+% xlabel('time [s]');
+% ylabel('crack length [mm/s]');
